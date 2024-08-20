9fa560e3b15bc1aff4dd000080de78f27e5c8bfd
Rhys Evans <wheresrhys@gmail.com>
2024-08-20 14:18:52 +0100

    refactor: use batchInsert to load data into db

--- src/lib/load-file.main.ts
+++ src/lib/load-file.main.ts
@@ -71,7 +71,17 @@ async function loadSingleFile(csvPath: string) {
   const knexDb = await getDBConnection();
 
   let rowNum = 1;
-  for (const row of csvData.data) {
+
+  // Note that I'm aware of the fact error handling is different now: before it
+  // would write lots of successful records, then exit on the first error, now
+  // it writes nothing unless no rows error.
+  // To avoid getting bogged down I've opted not to spend time on implementing
+  // something more similar to what was before. Would be something to discuss
+  // with the team to make sure the requirements were properly understood
+  // My gut feel is the right behaviour would be to log and alert on errors, but
+  // continue to write as much good data as possible, which is what I've implemented
+  // here
+  const transactions = csvData.data.map(row => {
     try {
       // Add more validation in the future?
       const spendDataRow = row as GovUKData;
@@ -79,7 +89,7 @@ async function loadSingleFile(csvPath: string) {
       // Some files have hundreds of rows with no data at the end, just commas.
       // It's safe to skip these.
       if (spendDataRow.Entity === "") {
-        continue;
+        return null
       }
 
       // TODO: We might have to support other date formats in the future
@@ -99,27 +109,26 @@ async function loadSingleFile(csvPath: string) {
        * but knex complains about sqlite not supporting default values.
        * It's ok to ignore that warning.
        */
-      // TODO: Use .batchInsert to speed this up, it's really slow with > 1000 transactions!
-      await knexDb<SpendTransaction>("spend_transactions").insert({
+      return {
         buyer_name: spendDataRow["Entity"],
         supplier_name: spendDataRow["Supplier"],
         amount: parseAmount(spendDataRow["Amount"]),
         transaction_timestamp: isoTsp,
-      });
+      };
 
-      ++rowNum;
-    } catch (e) {
+    } catch (error) {
       // Re-throw all errors, but log some useful info
-      console.error(`Failed to process row ${rowNum}: ${JSON.stringify(row)}`);
-      throw e;
+      console.error(`Failed to process row ${rowNum}: ${JSON.stringify(row)}`, error);
+      return null
     }
-  }
+  }).filter((row:(SpendTransaction | null)): row is SpendTransaction => Boolean(row))
+
+  await knexDb
+    .batchInsert('spend_transactions', transactions, 100)
 
   console.log("Finished writing to the DB.");
   await knexDb.destroy();
 }
-
-
 
 loadSingleFile("./sample_data/HMRC_spending_over_25000_for_August_2023.csv");
 loadSingleFile("./sample_data/Transparency_DfE_Spend_July_2023__1_.csv");
